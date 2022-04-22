84343bf41387d3d191f3a39b1ef3f38c58f2d30f
Roberto Pérez Guzmán <roperez@palaceresorts.com>
2022-04-21 21:59:26 -0500

    .build

--- build/Polimorfism.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const Rectangle_1 = require("./shapes/Rectangle");
-const rectangle = new Rectangle_1.default(20, 20, 5, 10);
-const circle = rectangle;
-console.log(circle.getRadio());
-//# sourceMappingURL=Polimorfism.js.map
--- build/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const Circle_1 = require("./shapes/Circle");
-const Rectangle_1 = require("./shapes/Rectangle");
-const c = new Circle_1.default(10, 5, 30);
-c.draw();
-const rectangle = new Rectangle_1.default(20, 20, 5, 10);
-rectangle.draw();
-//# sourceMappingURL=index.js.map
